71663dbf1265ba9ba397a188f841bc7de17056a6
kscarrot <kelstrait@icloud.com>
2025-03-27 13:39:38 +0800

    feat: 矩阵乘

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,21 @@
-import { add } from 'src/index'
+import { 矩阵乘1 } from 'src/index'
 
-describe('add', () => {
-  it('should add two numbers', () => {
-    expect(add(1, 2)).toBe(3)
+describe('矩阵操作', () => {
+  it('字符串矩阵乘', () => {
+    expect(
+      矩阵乘1(
+        [
+          ['a', 'b'],
+          ['c', 'd'],
+        ],
+        [
+          ['e', 'f'],
+          ['g', 'h'],
+        ],
+      ),
+    ).toStrictEqual([
+      ['ae+bg', 'ce+dg'],
+      ['af+bh', 'cf+dh'],
+    ])
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-export const add = (a: number, b: number) => a + b
+type 矩阵元 = number | string
+type 向量 = 矩阵元[]
+type 矩阵 = 向量[]
+
+const 元素加 = (a: 矩阵元, b: 矩阵元) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b
+  }
+  const aStr = a.toString()
+  const bStr = b.toString()
+  if (aStr && bStr) return `${aStr}+${bStr}`
+  return `${aStr}${bStr}`
+}
+
+const 元素乘 = (a: 矩阵元, b: 矩阵元) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a * b
+  }
+  const aStr = a.toString()
+  const bStr = b.toString()
+  return `${aStr}${bStr}`
+}
+
+const 向量乘 = (a: 向量, b: 向量) => {
+  return a.map((a, i) => 元素乘(a, b[i])).reduce((prev, curr) => 元素加(prev, curr), '')
+}
+
+const 行 = (matrix: 矩阵, index: number) => {
+  if (index >= matrix.length) throw Error('序数超过矩阵范围')
+
+  return matrix[index]
+}
+
+const 列 = (matrix: 矩阵, index: number) => {
+  if (index >= matrix.length) throw Error('序数超过矩阵范围')
+
+  const result: 矩阵元[] = []
+
+  matrix.forEach((row) => {
+    result.push(row[index])
+  })
+
+  return result
+}
+
+const 转置 = (matrix: 矩阵) => {
+  const result: 矩阵 = []
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    const column = 列(matrix, i)
+    result.push(column)
+  }
+
+  return result
+}
+
+export const 矩阵乘1 = (a: 矩阵, b: 矩阵) => 转置(a.map((行) => 转置(b).map((列) => 向量乘(行, 列))))
